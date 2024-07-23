9dcbdeb7775d6e161b19e61d810fa1f5b4987e58
AmalA <amal1212ar@gmail.com>
2024-07-23 07:35:05 +0300

    Initial commit

---
+++ lib/start_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget{
+
+   const StartScreen(this.startQuiz,{super.key});
+   final void Function() startQuiz;
+
+  @override
+  Widget build(context){
+    return Center(child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+      //const Image(image: AssetImage("asstes\images\quiz-logo.png"),width: 300,color: Color.fromARGB(174, 252, 251, 251),),
+      const SizedBox(height: 80,),
+      const Text("Learn Flutter the fun way!",style: TextStyle(color: Colors.white,fontSize: 24),),
+      const SizedBox(height: 30,),
+      OutlinedButton.icon( 
+        
+        onPressed: startQuiz,
+
+      style: OutlinedButton.styleFrom(
+        foregroundColor: Colors.white,
+      ),
+      icon: const Icon(Icons.arrow_right_alt),
+      label: const Text("Start Quiz")),
+
+      ],));
+  }
+
+}
